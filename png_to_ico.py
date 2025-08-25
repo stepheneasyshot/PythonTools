17593db364b8eda1225f0975a72803e8001f3306
stephenzhan <zhanfeng990927@gmail.com>
2025-08-25 09:27:26 +0800

    Update png_to_ico.py

--- png_to_ico.py
+++ png_to_ico.py
@@ -8,4 +8,4 @@ def png_to_ico(png_path, ico_path):
     image.save(ico_path, format='ICO', sizes=[(image.width, image.height)])
 
 # 调用函数并传入PNG图像路径和ICO文件路径
-png_to_ico('C:\\Users\\zhanf\\Desktop\\logo.png', 'output.ico')
+png_to_ico('C:\\Users\\zhanf\\Desktop\\logo.png', 'C:\\Users\\zhanf\\Desktop\\logo.ico')
